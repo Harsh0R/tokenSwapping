fdec912fc01af5c2d1091f0848a0961e3e08c022
Harsh <harshradadiya9999@gmail.com>
2024-10-11 11:13:10 +0530

    update

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
     amoy: {
       url: "https://rpc-amoy.polygon.technology",
       accounts: [
-        "1908fcfd6a15f4d574d203e47fd1dabec53e01ec39c434f36ac8c28df22e4688",
+        "",
       ],
       chainId: 80002,
     },
